49db7d2ce7c7eebc2ad537243dac4d94a766e72b
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-12-08 23:28:35 +0100

    init

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const qrIpt = document.querySelector(".form .url"),
-generateBtn = document.querySelector(".form .frames"),
-qrImg = document.querySelector(".qr-code img");
+  generateBtn = document.querySelector(".form .frames"),
+  qrImg = document.querySelector(".qr-code img");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
-    let qrValue = qrIpt.value.trim();
-    if(!qrValue || preValue === qrValue) return;
-    preValue = qrValue;
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=275x275&color=017dcc&data=${qrValue}`;
+  let qrValue = qrIpt.value.trim();
+  if (!qrValue || preValue === qrValue) return;
+  preValue = qrValue;
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=275x275&color=017dcc&data=${qrValue}`;
 });
 
 qrIpt.addEventListener("keyup", () => {
-    if(!qrIpt.value.trim()) {
-        wrapper.classList.remove("active");
-        preValue = "";
-    }
+  if (!qrIpt.value.trim()) {
+    wrapper.classList.remove("active");
+    preValue = "";
+  }
 });
